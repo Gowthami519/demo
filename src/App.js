208aae0f33c8079680d10872b4d145159bfef7c0
Gowthami <gowthami@pipesort.com>
2021-07-10 22:59:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Demo project
         </a>
       </header>
     </div>
